dc061c1ae1cfdb7c60ba52fc24ecceb41b5205e1
Christina Gerard <ChristinaGerard@Christinas-MBP.hsd1.ga.comcast.net>
2019-05-16 19:05:53 -0400

    added radio buttons on Modal. Only one can be selected

--- src/Components/add_target.js
+++ src/Components/add_target.js
@@ -7,16 +7,69 @@ const log = console.log;
 class AddTarget extends Component {
     state = {
         modal_form: false,
-        status_options: ['researching', 'pending approval', 'approved', 'declined']
+        status_option_checked: false,
+        status_option_selected: '',
+        status_options: [
+            { status: 'researching', checked: false },
+            { status: 'pending', checked: false },
+            { status: 'approved', checked: false },
+            { status: 'declined', checked: false }
+        ]
+
     }
 
     handleClick = (e) => {
         e.preventDefault();
-        log('adding target...');
-
         this.setState({
             modal_form: true
         })
+        // this.props.toggleAppModal();
+    }
+
+    updateStatusChecked = (selected_value) => {
+        // assign the status_options_copy in order to make changes to this.state.status_options
+        let status_options_copy = this.state.status_options;
+
+        // Loop through status_options_copy
+        status_options_copy.forEach(option => {
+            // if the status matches the selected value then assign checked as true
+            if (option.status === selected_value) {
+                option.checked = true;
+            } else {
+                option.checked = false;
+            }
+        });
+
+        // New value of status_options set
+        this.setState({ status_options: status_options_copy })
+    }
+
+    checkItem = (e, index) => {
+        let selected_value = this.state.status_options[index].status;
+
+        // If all have a checked status of false then update status_option_checked to true
+        // confirms if one status option has been checked
+        this.state.status_options.forEach(option => {
+            if (!option.checked) {
+                this.setState({
+                    status_option_checked: true
+                });
+            }
+        });
+
+        // May remove as the selected value can found when looping over 
+        // this.state.status_options to check which option has been checked as true
+        this.setState({
+            status_option_selected: selected_value
+        }, () => this.updateStatusChecked(selected_value));
+        
+
+        // May remove this.. unneeded at the momment
+        const { checked } = e.target;
+        log(checked);
+        log(index);
+        log(selected_value);
+        log(`----`);
     }
 
     render() {
@@ -30,7 +83,8 @@ class AddTarget extends Component {
                     Create new target
                 </Button>
 
-                {/* Modal form */}
+                {/* {this.props.children()} */}
+
                 <Modal
                     size="lg"
                     show={this.state.modal_form}
@@ -40,19 +94,29 @@ class AddTarget extends Component {
                     <Modal.Header closeButton>
                         <Modal.Title id="example-modal-sizes-title-lg">
                             Enter a target
-                    </Modal.Title>
+                        </Modal.Title>
                     </Modal.Header>
                     <Modal.Body>
+                        {/* Form */}
                         <Form>
                             <Form.Row>
-                                    <Form.Label>Status
-                                        <br />
-                                        {this.state.status_options.map(option => {
-                                            return (
-                                                    <Form.Check inline custom label={option} type='checkbox' id={option} key={option}/>
-                                            );
-                                        })}
-                                    </Form.Label>
+                                <Form.Label>Status
+                                    <br />
+                                    {this.state.status_options.map((option, index) => {
+                                        return (
+                                            <Form.Check
+                                                inline
+                                                custom
+                                                label={option.status}
+                                                id={option.status}
+                                                type='radio'
+                                                checked={option.checked}
+                                                key={option.status}
+                                                onChange={(e) => this.checkItem(e, index)}
+                                            />
+                                        );
+                                    })}
+                                </Form.Label>
                             </Form.Row>
                         </Form>
                     </Modal.Body>
