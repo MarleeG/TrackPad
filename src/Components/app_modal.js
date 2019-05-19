ece0a2bd103f17d65f7e618435da99255fec794e
Christina Gerard <ChristinaGerard@Christinas-MBP.hsd1.ga.comcast.net>
2019-05-19 09:24:05 -0400

    Form data can be sent to parent component

--- src/Components/app_modal.js
+++ src/Components/app_modal.js
@@ -1,44 +1,44 @@
-import React, { Component } from 'react';
-import { Modal, Form, Row, Col } from 'react-bootstrap';
+// import React, { Component } from 'react';
+// import { Modal, Form, Row, Col } from 'react-bootstrap';
 
-class AppModal extends Component {
+// class AppModal extends Component {
 
-    render() {
-        let hideModal = () => this.setState({ modal_form: false });
-        return (
-            <Modal
-                size="lg"
-                show={this.state.modal_form}
-                onHide={hideModal}
-                aria-labelledby="addTargetContainer"
-            >
-                <Modal.Header closeButton>
-                    <Modal.Title id="example-modal-sizes-title-lg">
-                        Enter a target
-            </Modal.Title>
-                </Modal.Header>
-                <Modal.Body>
-
-
-                    {/* Form */}
-                    <Form>
-                        <Form.Row>
-                            <Form.Label>Status
-                            <br />
-                                {this.state.status_options.map((option, index) => {
-                                    return (
-                                        <Form.Check inline custom label={option} type='radio' id={option} key={option} onChange={(e) => this.checkItem(e, index)} />
-                                    );
-                                })}
-                            </Form.Label>
-                        </Form.Row>
-                    </Form>
+//     render() {
+//         let hideModal = () => this.setState({ modal_form: false });
+//         return (
+//             <Modal
+//                 size="lg"
+//                 show={this.state.modal_form}
+//                 onHide={hideModal}
+//                 aria-labelledby="addTargetContainer"
+//             >
+//                 <Modal.Header closeButton>
+//                     <Modal.Title id="example-modal-sizes-title-lg">
+//                         Enter a target
+//             </Modal.Title>
+//                 </Modal.Header>
+//                 <Modal.Body>
 
 
-                </Modal.Body>
-            </Modal>
-        )
-    }
-}
+//                     {/* Form */}
+//                     <Form>
+//                         <Form.Row>
+//                             <Form.Label>Status
+//                             <br />
+//                                 {this.state.status_options.map((option, index) => {
+//                                     return (
+//                                         <Form.Check inline custom label={option} type='radio' id={option} key={option} onChange={(e) => this.checkItem(e, index)} />
+//                                     );
+//                                 })}
+//                             </Form.Label>
+//                         </Form.Row>
+//                     </Form>
 
-export default AppModal;
+
+//                 </Modal.Body>
+//             </Modal>
+//         )
+//     }
+// }
+
+// export default AppModal;
